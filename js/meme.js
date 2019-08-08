11c8fc037098556cfffdf0ef2147bfcddfec4c8a
Daniel Gilchrist <dan@tanda.co>
2019-08-08 16:48:48 +0100

    Add super secret mode

--- js/meme.js
+++ js/meme.js
@@ -5,15 +5,20 @@ document.addEventListener("keydown", handleKeyPress)
 const stopKey = 83 // s key
 const upKey = 38
 const downKey = 40
+const spaceKey = 32
 let interval = 0
 let spinning = false
 let speed = 0
+let secretMode = false
 
 function ʕಠᴥಠʔ (ᕕ〳ಠل͜ಠ〵ᕗ) {
   const ಠωಠ = document.createElement("img")
   ಠωಠ.setAttribute("src", "./images/degenerate.png")
   ಠωಠ.style.left = `${ᕕ〳ಠل͜ಠ〵ᕗ.clientX - 60}px`
   ಠωಠ.style.top = `${ᕕ〳ಠل͜ಠ〵ᕗ.clientY - 100}px`
+  if (secretMode) {
+    ಠωಠ.style["transform-origin"] = generateTransformOrigin()
+  }
 
   const 〳ಠʖಠ〵 = document.createElement("audio")
   〳ಠʖಠ〵.setAttribute("src", `./audio/degenerate${Math.floor(Math.random() * 4) + 1}.m4a`)
@@ -31,16 +36,29 @@ function handleKeyPress (e) {
     if (!spinning) { spin() }
   } else if (e.keyCode == stopKey) {
     stop()
+  } else if (e.keyCode == spaceKey) {
+    secretMode = !secretMode
+
+    const images = document.body.getElementsByTagName("img")
+    Array.prototype.forEach.call(images, image => {
+      let transformValue = "0"
+
+      if (secretMode) {
+        transformValue = generateTransformOrigin()
+      }
+
+      image.style["transform-origin"] = transformValue
+    })
   }
 }
 
 function spin () {
-  var images = document.body.getElementsByTagName("img")
+  const images = document.body.getElementsByTagName("img")
   let rot = 0
   interval = setInterval(() => {
     rot += speed
-    Array.prototype.forEach.call(images, el => {
-      el.style.transform = `rotate(${rot}deg)`
+    Array.prototype.forEach.call(images, image => {
+      image.style.transform = `rotate(${rot}deg)`
     })
   }, 1)
   spinning = true
@@ -48,10 +66,14 @@ function spin () {
 
 function stop () {
   clearInterval(interval)
-  var els = document.body.getElementsByTagName("img")
-  Array.prototype.forEach.call(els, el => {
-    el.style.transform = "rotate(0deg)"
+  const images = document.body.getElementsByTagName("img")
+  Array.prototype.forEach.call(images, image => {
+    image.style.transform = "rotate(0deg)"
   })
   spinning = false
-  speed = 1
+  speed = 0
+}
+
+function generateTransformOrigin(element) {
+  return `${Math.random() * 100}% ${Math.random() * 100}%`
 }
